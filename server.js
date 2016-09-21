467ce0b1d12f0ffa1eac389739148c48a588c913
Alex Goncalves <agoncalves4141@gmail.com>
2016-09-21 14:26:57 -0400

    Standardized directory access

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var server = express();
 server.use(express.static(__dirname + '/res'));
 
 server.get('/', function(request, response) {
-	response.sendFile('./index.html', {root: '../Capstone_Class'});
+	response.sendFile('./index.html', {root: '../Treet'});
 });
 
 server.get('/home', function(request, response) {
-	response.sendFile('./index.html', {root: '../Capstone_Class'});
+	response.sendFile('./index.html', {root: '../Treet'});
 });
 
 server.listen(3000, function() {
