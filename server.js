d4d1b94950159ea07e25be8a63580797adf28ffc
Alex Goncalves <agoncalves4141@gmail.com>
2016-09-25 16:54:43 -0400

    Added non-permanent database connection functionality

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pgp = require("pg-promise")(/*options*/);
+var db = pgp("postgres://postgres:marist@localhost:3000/TreetDB");
 var server = express();
 
 server.use(express.static(__dirname + '/res'));
@@ -11,7 +13,19 @@ server.get('/home', function(request, response) {
 	response.sendFile('./index.html', {root: '../Treet'});
 });
 
-server.listen(3000, function() {
-	console.log('Example app listening on port 3000!');
+server.listen(3001, function() {
+	console.log('Example app listening on port 3001!');
 });
 
+/*
+Example
+db.any("SELECT FirstName FROM Customers", [true])
+    .then(function (data) {
+    	//for(var d in data){
+        	console.log("DATA:", data);
+    	//}
+    })
+    .catch(function (error) {
+        console.log("ERROR:", error);
+    });
+*/
